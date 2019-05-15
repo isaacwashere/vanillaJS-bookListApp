7229eb1f6de7f97a361dcf23735a9de97cffee51
Isaac Curtiss <ijcurtiss7@gmail.com>
2019-05-14 17:46:15 -0700

    mockup/sketching of JS structure

---
+++ app.js
@@ -0,0 +1,39 @@
+//Book class
+//Represents a book, 
+//Each time a book is created, a book object will be instantiated
+
+
+
+
+//UI Class
+//Handle UI Tasks
+//when a book is displayed or removed or show an alert 
+
+
+
+
+//Store Class
+//Handles and takes care of storage
+//Right now this storage will be local storage i.e. in the browser
+
+
+
+
+//Events: Display
+//to display books and show them in the list 
+
+
+
+
+//Event: Add
+//to add a book to the list 
+
+
+
+
+//Event: Remove
+//to remove a book (in the UI and local storage)
+
+
+
+
